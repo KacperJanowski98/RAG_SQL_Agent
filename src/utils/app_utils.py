a0c38c2929f9a833825d2e1b72d0479b1335d964
KacperJanowski98 <Janowski.Kacper1995@wp.pl>
2025-01-02 10:34:29 +0100

    Added utils for app

---
+++ src/utils/app_utils.py
@@ -0,0 +1,13 @@
+import os
+from pyprojroot import here
+
+
+def create_directory(directory_path: str) -> None:
+    """
+    Create a directory if it does not exist.
+
+    Parameters:
+        directory_path (str): The path of the directory to be created.
+    """
+    if not os.path.exists(here(directory_path)):
+        os.makedirs(here(directory_path))
